ac8c5b10d160b82ff814bc4fffbdea42f6998f1f
Maxx <maximenorval@gmail.com>
2026-07-31 11:50:42 +0200

    formatting general concept comment

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ GENERAL CONCEPT:
         - Uses a subagent to find out the main language spoken in the city
         - Uses a subagent to get a summary translated in the city language (if it's not English) -> Translated Summary
         - Uses a subagent to get an audio summary (= audio version of the Translated Summary if it exists / of the Summary if not) -> Audio Summary
-        - Displays the Google Image (if exists), the Translated Summary (if exists) + the Summary in markdown, and an audio player with the Audio Summary
+        - Displays:
+            * the Google Image (if exists)
+            * the Translated Summary (if exists)
+            * the Summary in markdown
+            * an audio player with the Audio Summary
     - If scraping the Wikipedia page failed:
         - Displays a message to the user, saying so
 """

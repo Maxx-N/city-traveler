68a55b800b0e7a10a2d046782ec1ca90d538bb6f
Maxx <maximenorval@gmail.com>
2026-07-31 12:09:37 +0200

    writing a method to get the first Wikipedia image URL on a subject

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ GENERAL CONCEPT:
 - The LLM:
     - Uses a tool to scrap the English Wikipedia page
     - If scraping the Wikipedia page worked:
-        - Uses a tool to get the first Google image -> Google Image
+        - Uses a tool to get the first Wikipedia image -> Wikipedia Image
         - Uses a subagent to summarize the Wikipedia page in a concise, structured and compelling way -> Summary
         - Uses a subagent to find out the main language spoken in the city
         - Uses a subagent to get a summary translated in the city language (if it's not English) -> Translated Summary
         - Uses a subagent to get an audio summary (= audio version of the Translated Summary if it exists / of the Summary if not) -> Audio Summary
         - Displays:
-            * the Google Image (if exists)
+            * the Wikipedia Image (if exists)
             * the Translated Summary (if exists)
             * the Summary in markdown
             * an audio player with the Audio Summary
